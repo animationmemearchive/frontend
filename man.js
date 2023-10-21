efd5864113e106e7ffbce5aa15d7af79391e6877
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-10-21 03:52:00 -0700

    get json for each video from backend

--- man.js
+++ man.js
@@ -1,9 +1,17 @@
+// the file is supposed to be named main.js but i typoed and it's funnier this way
 window.onload = function () {
     const videogrid = document.getElementById('video-grid');
     fetch('http://localhost:3000/api/media')
         .then(response => response.json())
         .then(data => {
-            data.forEach(entry => {
+            data.forEach(async entry => {
+                let fileInfo;
+                await fetch(`http://localhost:3000/api/media/${entry.title}`)
+                .then(response => response.json())
+                .then(data => {
+                    fileInfo = data;
+                });
+
                 const videoContainer = document.createElement('div');
                 videoContainer.classList.add('videocontainer');
                 videoContainer.style.textAlign = 'center';
@@ -24,10 +32,14 @@ window.onload = function () {
                 const link = document.createElement('a');
                 link.href = entry.fullPath;
                 link.target = '_blank';
-                link.textContent = entry.fullPath;
+                link.textContent = fileInfo.title;
+
+                const author = document.createElement('p');
+                author.textContent = `by ${fileInfo.author}`;
 
                 videoContainer.appendChild(document.createElement('br'));
                 videoContainer.appendChild(link);
+                videoContainer.appendChild(author)
 
                 videogrid.appendChild(videoContainer);
             });
