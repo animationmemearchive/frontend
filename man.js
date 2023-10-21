d251e9fb5187c063c64239736274085b5a152580
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-10-21 14:17:55 -0700

    Tailwind, no more DDOSing the backend

--- man.js
+++ man.js
@@ -5,13 +5,6 @@ window.onload = function () {
         .then(response => response.json())
         .then(data => {
             data.forEach(async entry => {
-                let fileInfo;
-                await fetch(`http://localhost:3000/api/media/${entry.title}`)
-                .then(response => response.json())
-                .then(data => {
-                    fileInfo = data;
-                });
-
                 const videoContainer = document.createElement('div');
                 videoContainer.classList.add('videocontainer');
                 videoContainer.style.textAlign = 'center';
@@ -32,10 +25,10 @@ window.onload = function () {
                 const link = document.createElement('a');
                 link.href = entry.fullPath;
                 link.target = '_blank';
-                link.textContent = fileInfo.title;
+                link.textContent = entry.fileInfo.title;
 
                 const author = document.createElement('p');
-                author.textContent = `by ${fileInfo.author}`;
+                author.textContent = `by ${entry.fileInfo.author}`;
 
                 videoContainer.appendChild(document.createElement('br'));
                 videoContainer.appendChild(link);
