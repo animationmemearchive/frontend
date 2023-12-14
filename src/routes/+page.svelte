f2a94052719530fdc305d1e67babb121cf4989aa
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2023-12-14 12:46:06 -0800

    actually functional

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,5 @@
 <h1>animation memes</h1>
 {#each data.creators.data as creator}
     <p>{creator.attributes.Name}</p>
+    <a href="/creators/{creator.id}"><button>go</button></a>
 {/each}
---
+++ src/routes/creators/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let data;
+
+    console.log(data);
+</script>
+
+{#each data.videos.data as video}
+    <!-- svelte-ignore a11y-media-has-caption -->
+    <video width="320" height="240" controls>
+        <source src={'http://100.109.118.69:1337' + video.attributes.video.data.attributes.url} type="video/mp4" />
+    </video>
+    <p>{video.attributes.title}</p>
+{/each}
