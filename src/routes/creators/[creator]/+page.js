f31e5ba1c55adfe1de4d3a54e8a283fc209d1a86
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-01-19 19:17:39 -0800

    add a name matching ability

--- src/routes/creators/[creator]/+page.js
+++ src/routes/creators/[creator]/+page.js
@@ -2,11 +2,23 @@ import { databases } from '$lib/appwrite.js';
 import { Query } from 'appwrite';
 
 export async function load({ params }) {
+    let creator = params.creator;
+
+    let search = await databases.listDocuments(
+        "videos",
+        "65a9b15650d2d14667ec",
+        [
+            Query.equal("name", params.creator),
+        ]
+    );
+
+    if (search.documents.length != 0) creator = search.documents[0].$id;
+
     let promise = await databases.listDocuments(
         "videos",
         "typh",
         [
-            Query.equal("creators", params.creator),
+            Query.equal("creators", creator),
             Query.orderDesc('createdDate'),
             Query.limit(100), // TODO: Pagination
         ]
