dbd163889e6b599cc1cf2455217200dd24564e4b
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2024-01-22 13:18:28 -0800

    add pink to creator pages

--- src/routes/creators/[creator]/+page.svelte
+++ src/routes/creators/[creator]/+page.svelte
@@ -2,30 +2,64 @@
     import { PUBLIC_API_ENDPOINT, PUBLIC_PROJECT_ID } from '$env/static/public';
 
     export let data;
+
+    import '@appwrite.io/pink';
+    import '@appwrite.io/pink-icons';
 </script>
 
-<h1>{data.creator.name}</h1>
-<a href={data.creator.sociallink}><h3>{data.creator.sociallink}</h3></a>
+<div class="container">
+    <h1 class="heading-level-1">{data.creator.name}</h1>
+    <a href={data.creator.sociallink}><h4 class="eyebrow-heading-3 link">{data.creator.sociallink}</h4></a>
 
-<a href="/"><button>home</button></a><br /><br />
+    <a href="/">
+        <button class="button is-secondary" aria-label="Go Home">
+            <span class="icon-home" aria-hidden="true"></span>
+            <span class="text">Home</span>
+        </button></a>
+    <br /> <br />
 
-<div class="video-grid">
-    {#each data.videos as video}
-        <!-- svelte-ignore a11y-media-has-caption -->
-        <div class="videocontainer" style="text-align: center;">
-            <video
-                width="320"
-                height="240"
-                controls
-                preload="none"
-                poster={`${PUBLIC_API_ENDPOINT}/storage/buckets/thumbnails/files/${video.thumbID}/view?project=${PUBLIC_PROJECT_ID}`}>
-                <source
-                    src={`${PUBLIC_API_ENDPOINT}/storage/buckets/videos/files/${video.videoID}/view?project=${PUBLIC_PROJECT_ID}`}
-                    type="video/mp4" />
-            </video>
-            <br />
-            <a href={`${PUBLIC_API_ENDPOINT}/storage/buckets/videos/files/${video.videoID}/view?project=${PUBLIC_PROJECT_ID}`} target="_blank"
-                >{video.title}</a>
-        </div>
-    {/each}
+    <ul class="grid-box" style="--grid-gap:1rem; --grid-item-size:20rem; --grid-item-size-small-screens:8rem;">
+        {#each data.videos as video}
+            <!-- svelte-ignore a11y-media-has-caption -->
+            <li>
+                <div class="card">
+                    <video
+                        width="320"
+                        height="240"
+                        controls
+                        preload="none"
+                        poster={`${PUBLIC_API_ENDPOINT}/storage/buckets/thumbnails/files/${video.thumbID}/view?project=${PUBLIC_PROJECT_ID}`}>
+                        <source
+                            src={`${PUBLIC_API_ENDPOINT}/storage/buckets/videos/files/${video.videoID}/view?project=${PUBLIC_PROJECT_ID}`}
+                            type="video/mp4" />
+                    </video>
+                    <br />
+                    <a
+                        href={`${PUBLIC_API_ENDPOINT}/storage/buckets/videos/files/${video.videoID}/view?project=${PUBLIC_PROJECT_ID}`}
+                        target="_blank">{video.title}</a>
+                </div>
+            </li>{/each}
+    </ul>
+
+    <div class="video-grid">
+        {#each data.videos as video}
+            <!-- svelte-ignore a11y-media-has-caption -->
+            <div class="videocontainer" style="text-align: center;">
+                <video
+                    width="320"
+                    height="240"
+                    controls
+                    preload="none"
+                    poster={`${PUBLIC_API_ENDPOINT}/storage/buckets/thumbnails/files/${video.thumbID}/view?project=${PUBLIC_PROJECT_ID}`}>
+                    <source
+                        src={`${PUBLIC_API_ENDPOINT}/storage/buckets/videos/files/${video.videoID}/view?project=${PUBLIC_PROJECT_ID}`}
+                        type="video/mp4" />
+                </video>
+                <br />
+                <a
+                    href={`${PUBLIC_API_ENDPOINT}/storage/buckets/videos/files/${video.videoID}/view?project=${PUBLIC_PROJECT_ID}`}
+                    target="_blank">{video.title}</a>
+            </div>
+        {/each}
+    </div>
 </div>
