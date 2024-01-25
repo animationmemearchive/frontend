3bd9d663d3634589bdbff2672f2e0ee0a46325de
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-01-24 19:04:03 -0800

    beg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script>
     import './app.css';
+
+    import '@appwrite.io/pink';
+    import '@appwrite.io/pink-icons';
 </script>
+
+<section class="alert is-action is-info">
+    <div class="alert-grid">
+        <span class="icon-currency-dollar" aria-hidden="true"></span>
+        <div class="alert-content">
+            <h6 class="alert-title">Video hosting isn't cheap!</h6>
+            <p class="alert-message">Donate to allow the website features like more creators, faster load-times, and no crashes!</p>
+        </div>
+        <div class="alert-buttons u-flex u-gap-16 u-cross-child-center">
+            <button class="button is-secondary is-full-width-mobile">
+                <a href="https://donate.stripe.com/fZe29Je16cuc3sI4gh" target="_blank"><span class="text">Donate</span></a>
+            </button>
+        </div>
+    </div>
+</section>
 
 <slot />
