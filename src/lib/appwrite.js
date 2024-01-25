88ff0a1e75d27e26462c651f04bd71abdfe9cfe6
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2024-01-24 16:08:46 -0800

    this was not supposed to be on main
    This reverts commit 63142f6acd64b17201015fad5e0cc25e07b97ed7.

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,15 +1,13 @@
-import { Client, Databases, Account } from 'appwrite';
+import { Client, Databases } from 'appwrite';
 import { PUBLIC_API_ENDPOINT, PUBLIC_PROJECT_ID } from '$env/static/public';
 
 const client = new Client();
 
 const databases = new Databases(client);
 
-const account = new Account(client);
-
 client
     .setEndpoint(PUBLIC_API_ENDPOINT) // Your API Endpoint
     .setProject(PUBLIC_PROJECT_ID) // Your project ID
     ;
 
-export { databases, account };
+export { databases };
