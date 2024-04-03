fe02f88c7a60dcbcb1900a4d127232f5920da088
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2024-04-03 14:14:02 -0700

    sunset notice

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,17 +5,20 @@
     import '@appwrite.io/pink-icons';
 </script>
 
-<section class="alert is-action is-info">
+<section class="alert is-action is-danger">
     <div class="alert-grid">
-        <span class="icon-currency-dollar" aria-hidden="true"></span>
+        <span class="icon-info" aria-hidden="true"></span>
         <div class="alert-content">
-            <h6 class="alert-title">Video hosting isn't cheap!</h6>
-            <p class="alert-message">Donate to allow the website features like more creators, faster load-times, and no crashes!</p>
+            <h6 class="alert-title">Goodbye!</h6>
+            <p class="alert-message">
+                i'm no longer interested in working on or paying for this project. animation meme archive will be sunsetting in the very near
+                future. cya &lt;3
+            </p>
         </div>
         <div class="alert-buttons u-flex u-gap-16 u-cross-child-center">
-            <a href="https://donate.stripe.com/fZe29Je16cuc3sI4gh" target="_blank"
+            <a href="https://web.archive.org/web/20240402083214/https://animationmemes.arti.lol/" target="_blank"
                 ><button class="button is-secondary is-full-width-mobile">
-                    <span class="text">Donate</span>
+                    <span class="text">view on archive.org</span>
                 </button></a>
         </div>
     </div>
